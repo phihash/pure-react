c543b1ed1e21667578c937401c2b28299c087917
phihash <insharp0220@gmail.com>
2026-05-21 01:44:58 +0900

    すすむ

--- src/App.tsx
+++ src/App.tsx
@@ -13,14 +13,64 @@ function App() {
     <>
       <section id="center">
         <h2>進捗</h2>
-        <h3>UIの記述</h3>
+
+        <ul>
+          <li>
+            <details>
+              <summary>初めてのコンポーネント✅</summary>
+              <p>
+                Reactコンポーネントとは、簡単にいうと、マークアップを添えられるJavaScript関数である
+              </p>
+            </details>
+          </li>
+          <li>コンポーネントのインポートとエクスポート</li>
+          <li>JSX でマークアップを記述する</li>
+          <li>JSX に波括弧で JavaScript を含める</li>
+          <li>コンポーネントに props を渡す</li>
+          <li>条件付きレンダー</li>
+          <li>リストのレンダー</li>
+          <li>コンポーネントを純粋に保つ</li>
+          <li>UI をツリーとして理解する</li>
+        </ul>
+
+        <h3>インタラクティビティの追加</h3>
+        <ul>
+          <li>イベントへの応答</li>
+          <li>state：コンポーネントのメモリ</li>
+
+          <li>レンダーとコミット</li>
+          <li>state はスナップショットである</li>
+          <li>一連の state の更新をキューに入れる</li>
+          <li>state 内のオブジェクトの更新</li>
+          <li>state 内の配列の更新</li>
+        </ul>
+
+        <h3>state の管理</h3>
+        <ul>
+          <li>state を使って入力に反応する</li>
+          <li>state 構造の選択</li>
+          <li>コンポーネント間で state を共有する</li>
+          <li>state の保持とリセット</li>
+          <li>state ロジックをリデューサに抽出する</li>
+          <li>コンテクストで深くデータを受け渡す</li>
+          <li>リデューサとコンテクストでスケールアップ</li>
+        </ul>
+
+        <h3>避難ハッチ</h3>
+        <ul>
+          <li>ref で値を参照する</li>
+          <li>ref で DOM を操作する</li>
+          <li>エフェクトを使って同期を行う</li>
+          <li>そのエフェクトは不要かも</li>
+          <li>リアクティブなエフェクトのライフサイクル</li>
+          <li>エフェクトからイベントを分離する</li>
+          <li>エフェクトから依存値を取り除く</li>
+          <li>カスタムフックでロジックを再利用する</li>
+        </ul>
+        <h6>スロットル例</h6>
         <Throttle />
         <RespondingToEvents />
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
+
         <div>
           <h1>Get started</h1>
           <p>
